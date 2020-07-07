ebe97491902189b8d9b96a7ce769d7a8a7fd6bf2
Griffin <chemist_khnu@rambler.ru>
2020-07-07 23:59:29 +0300

    JS HW12 draft 2

--- src/handlers.js
+++ src/handlers.js
@@ -1,12 +1,17 @@
 import countryTemplate from './templates/country.hbs';
-import * as PNotify from '@pnotify/core'
+import * as PNotify from '@pnotify/core';
+import * as _ from 'lodash';
+import {
+  fetchCountries
+} from './fetchCountries';
 
-let listArray = [];
+
+let request = '';
 
 const res = {
   list: document.querySelector('.country-list'),
   country: document.querySelector('.country-info'),
-  err: document.querySelector('.error')
+  input: document.querySelector('#js-input-field')
 }
 
 // Country list (up to 10)
@@ -17,25 +22,17 @@ const showCountryList = function (listArray) {
 
 // Country detail (one result)
 const showCountryDetail = function (listArray) {
-  const countryDetailCode =  countryTemplate(listArray[0]);
+  const countryDetailCode = countryTemplate(listArray[0]);
   res.country.insertAdjacentHTML('afterbegin', countryDetailCode);
 };
 
-export const handleInput = function (listArray) {
-  const l = listArray.length
-  switch (l) {
-    case 1:
+const handleInput = function (listArray) {
+  const check = listArray.length;
+  switch (true) {
+    case (check === 1):
       showCountryDetail(listArray);
       break;
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-    case 6:
-    case 7:
-    case 8:
-    case 9:
-    case 10:
+    case (check>=2 && check <= 10):
       showCountryList(listArray);
       break;
     default:
@@ -45,4 +42,18 @@ export const handleInput = function (listArray) {
       });
       break;
   };
+};
+
+const handleRequest = function () {
+  request = res.input.value;
+  res.list.innerHTML='';
+  res.country.innerHTML='';
+  fetchCountries(request).then(data => {
+    console.log(data);
+    handleInput(data)
+  });
+
 }
+
+const handleRequestDebounce = _.debounce(handleRequest, 500);
+res.input.addEventListener('input', handleRequestDebounce);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import './styles.css';
 import './fetchCountries.js';
 import './handlers.js';
-import './request_handle';
 import '@pnotify/core/dist/BrightTheme.css';
